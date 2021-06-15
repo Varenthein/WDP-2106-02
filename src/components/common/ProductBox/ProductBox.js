cb4a066b15e113a4fdb7a1bdf597ff8be52d83c0
Natalia Szlapka <n.machalica@gmail.com>
2021-06-15 17:27:27 +0200

    Add StarsRating component and its functionality

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types';
 
 import styles from './ProductBox.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faStar,
-  faExchangeAlt,
-  faShoppingBasket,
-} from '@fortawesome/free-solid-svg-icons';
-import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faExchangeAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
+
+import StarsRating from '../StarsRating/StarsRating';
 
 const ProductBox = ({ name, price, promo, stars }) => (
   <div className={styles.root}>
@@ -25,15 +23,7 @@ const ProductBox = ({ name, price, promo, stars }) => (
     <div className={styles.content}>
       <h5>{name}</h5>
       <div className={styles.stars}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#'>
-            {i <= stars ? (
-              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-            ) : (
-              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-            )}
-          </a>
-        ))}
+        <StarsRating stars={stars} />
       </div>
     </div>
     <div className={styles.line}></div>
