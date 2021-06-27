6661a7de2fb244d324229066578a56a82d60ec93
Bartek Mucha <bartekmucha43@gmail.com>
2021-06-27 12:02:26 +0200

    Add complete stickyBar

--- src/components/features/StickyBar/StickyBar.js
+++ src/components/features/StickyBar/StickyBar.js
@@ -3,26 +3,34 @@ import styles from './StickyBar.module.scss';
 import Button from '../../common/Button/Button';
 import PropTypes from 'prop-types';
 
-const StickyBar = ({ compareProducts }) => (
-  <div className={styles.container}>
-    <div className='container'>
-      <div className='row'>
-        <div className='col-10'>
-          <div className='row'>
-            {/* {compareProducts.map(product => (
-              <img key={product} src={compareProducts} className={styles.image} alt='compareProduct'></img>
-            ))} */}
+const StickyBar = ({ compareProducts }) =>
+  compareProducts.length > 0 ? (
+    <div className={styles.container}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-10'>
+            <div className='row'>
+              {compareProducts.map(product => (
+                <img
+                  key={product}
+                  src={product}
+                  className={styles.image}
+                  alt='compareProduct'
+                ></img>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className='col-2'>
-          <Button className={styles.btn} variant={'outline'}>
-            Compare
-          </Button>
+          <div className='col-2'>
+            <Button className={styles.btn} variant={'outline'}>
+              Compare
+            </Button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  ) : (
+    ''
+  );
 
 StickyBar.propTypes = {
   compareProducts: PropTypes.array,
