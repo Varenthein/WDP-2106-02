97013457bfeca3f644598865b41d5e25e893690e
Bartek Mucha <bartekmucha43@gmail.com>
2021-06-27 17:57:32 +0200

    Add selectors for product - error

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,6 +5,15 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+// export const getLimitedProducts = ({ products }, limit) =>
+//   products.filter(item => item === limit);
+
+// export const sortAscByParam = ({ products }, param) =>
+//   products.mixedNumericArray.sort(param);
+
+// export const sortDescByParam = ( { products}, param) =>
+
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
