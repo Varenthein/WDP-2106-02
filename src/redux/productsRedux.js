26e3431cd4a03e6761670a3c2891d857a10062df
Bartek Mucha <bartekmucha43@gmail.com>
2021-06-28 19:24:49 +0200

    Add selectors

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,14 +5,14 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
-// export const getLimitedProducts = ({ products }, limit) =>
-//   products.filter(item => item === limit);
+export const getLimitedProducts = ({ products }, limit) =>
+  products.slice(limit);
 
-// export const sortAscByParam = ({ products }, param) =>
-//   products.mixedNumericArray.sort(param);
+export const sortAscByParam = ({ products }, param) =>
+  products.sort((a, b) => a.price - b.price);
 
-// export const sortDescByParam = ( { products}, param) =>
-
+export const sortDescByParam = ( { products}, param) =>
+  products.sort((a , b) => b.price - a.price);
 
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
